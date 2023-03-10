f7f5c55b0898d6d72929004038364ee7d782da5d
aditya <adityatandulkar20@gmail.com>
2023-03-10 11:57:56 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,26 @@ import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 
 
-const getData =  () =>{
-  const rawData =  fetch('https://jsonplaceholder.typicode.com/todos')
-  const data =  rawData.json()
+const getData =  async () =>{
+  const rawData =  await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await rawData.json()
   return data
 }
+
+
 const App = () => {
+
   const [todos,setTodos] = useState([])
   useEffect(()=>{
-    getData().then(data=> setTodos(data) )
+    getData().then(data=> {
+      setTodos(data)
+     } )
   },[])
+
+
   return (
     <div id="main">
-      {todos.map(todo=> <div id={`todo-${todo.id}`}>{todo.title}</div>)}
+      {todos.map(todo=> <div key={todo.id} id={`todo-${todo.id}`}>{todo.title}</div>)}
     </div>
   )
 }
